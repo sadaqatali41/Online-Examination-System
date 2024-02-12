360e8dc0d0be58758c6d5f2ded006535f2489591
Sadaqat Ali <sadaqatali890@gmail.com>
2024-02-12 22:35:43 +0530

    student data population is done for high school, intermediate, graduation

--- admin/student.php
+++ admin/student.php
@@ -240,7 +240,7 @@ $user_data = $_SESSION['user_data'];
         case 'edit':
             if(filter_has_var(INPUT_GET, 'id')) {
                 $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
-                $stmt = $conn->prepare("SELECT st.id, st.fname, st.lname, st.email, st.password, st.phone, st.gender, st.course_id, cs.course_name, 
+                $stmt = $conn->prepare("SELECT st.id, st.fname, st.lname, st.email, st.password, st.phone, st.gender, st.course_id, cs.course_name, cc.cc_code, 
                 st.center_id, ce.center_name, st.city, st.pin, st.state, st.country, c.name AS city_nm, s.name AS state_nm, ct.name AS country_nm, 
                 st.pin, st.address, st.avatar, st.status, st.dob 
                 FROM students st 
@@ -249,6 +249,7 @@ $user_data = $_SESSION['user_data'];
                 LEFT JOIN cities c ON c.id=st.city 
                 LEFT JOIN states s ON s.id=st.state 
                 LEFT JOIN countries ct ON ct.id=st.country 
+                LEFT JOIN course_category cc ON cc.id=cs.cc_id
                 WHERE st.id=?");
                 $stmt->bind_param("i", $id);
                 $stmt->execute();
@@ -262,6 +263,30 @@ $user_data = $_SESSION['user_data'];
             } else {
                 header('Location: student.php');
             }
+
+            #high school data
+            $sths = $conn->prepare("SELECT hs.* FROM highschool hs WHERE hs.student_id=?");
+            $sths->bind_param("i", $id);
+            $sths->execute();
+            $reshs = $sths->get_result();
+            $sths->close();
+            $rowhs = $reshs->fetch_assoc();
+
+            #intermediate data
+            $stim = $conn->prepare("SELECT im.* FROM intermediate im WHERE im.student_id=?");
+            $stim->bind_param("i", $id);
+            $stim->execute();
+            $resim = $stim->get_result();
+            $stim->close();
+            $rowim = $resim->fetch_assoc();
+
+            #intermediate data
+            $stgd = $conn->prepare("SELECT gd.* FROM graduation gd WHERE gd.student_id=?");
+            $stgd->bind_param("i", $id);
+            $stgd->execute();
+            $resgd = $stgd->get_result();
+            $stgd->close();
+            $rowgd = $resgd->fetch_assoc();
         ?>
             <section class="content-header">
                 <h1>Edit Student</h1>
@@ -296,10 +321,12 @@ $user_data = $_SESSION['user_data'];
                                                     </li>
                                                     <li>
                                                         <a href="#intermediate" data-toggle="tab" aria-expanded="false">Intermediate</a>
-                                                    </li>                                                    
+                                                    </li>
+                                                    <?php if($row['cc_code'] === 'PG') : ?>
                                                     <li>
                                                         <a href="#graduation" data-toggle="tab" aria-expanded="false">Graduation</a>
-                                                    </li>                                                    
+                                                    </li>
+                                                    <?php endif; ?>
                                                 </ul>
                                             </div>
                                         </div>
@@ -443,17 +470,122 @@ $user_data = $_SESSION['user_data'];
                                                 </div>
                                                 <div class="tab-pane" id="high_school">
                                                     <div class="form-wrapper">
-                                                        <h1>High School</h1>
+                                                        <div class="row">
+                                                            <div class="col-md-6">
+                                                                <div class="form-group">
+                                                                    <label for="school_name">School Name</label>
+                                                                    <input type="text" name="school_name" id="school_name" value="<?= ($rowhs['school_name'] ?? ''); ?>" class="form-control" placeholder="School Name with address" autocomplete="off">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-md-2">
+                                                                <div class="form-group">
+                                                                    <label for="board_name">Board Name</label>
+                                                                    <input type="text" name="board_name" id="board_name" value="<?= ($rowhs['board_name'] ?? ''); ?>" class="form-control" placeholder="Board Name" autocomplete="off">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-md-2">
+                                                                <div class="form-group">
+                                                                    <label for="roll_no">Roll No</label>
+                                                                    <input type="text" name="roll_no" id="roll_no" value="<?= ($rowhs['roll_no'] ?? ''); ?>" class="form-control" placeholder="Roll No" autocomplete="off">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-md-2">
+                                                                <div class="form-group">
+                                                                    <label for="yop">Year of Passing</label>
+                                                                    <input type="text" name="yop" id="yop" value="<?= ($rowhs['yop'] ?? ''); ?>" class="form-control" placeholder="Year of Passing" autocomplete="off">
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="row">
+                                                            <div class="col-md-2">
+                                                                <div class="form-group">
+                                                                    <label for="percent">Passing %</label>
+                                                                    <input type="text" name="percent" id="percent" value="<?= ($rowhs['percent'] ?? ''); ?>" class="form-control" placeholder="Passing %" autocomplete="off">
+                                                                </div>
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                 </div>
                                                 <div class="tab-pane" id="intermediate">
                                                     <div class="form-wrapper">
-                                                        <h1>Intermediate</h1>
+                                                        <div class="row">
+                                                            <div class="col-md-6">
+                                                                <div class="form-group">
+                                                                    <label for="college_name">College Name</label>
+                                                                    <input type="text" name="college_name" id="college_name" value="<?= ($rowim['college_name'] ?? ''); ?>" class="form-control" placeholder="College Name with address" autocomplete="off">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-md-2">
+                                                                <div class="form-group">
+                                                                    <label for="im_board_name">Board Name</label>
+                                                                    <input type="text" name="im_board_name" id="im_board_name" value="<?= ($rowim['board_name'] ?? ''); ?>" class="form-control" placeholder="Board Name" autocomplete="off">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-md-2">
+                                                                <div class="form-group">
+                                                                    <label for="im_roll_no">Roll No</label>
+                                                                    <input type="text" name="im_roll_no" id="im_roll_no" value="<?= ($rowim['roll_no'] ?? ''); ?>" class="form-control" placeholder="Roll No" autocomplete="off">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-md-2">
+                                                                <div class="form-group">
+                                                                    <label for="im_yop">Year of Passing</label>
+                                                                    <input type="text" name="im_yop" id="im_yop" value="<?= ($rowim['yop'] ?? ''); ?>" class="form-control" placeholder="Year of Passing" autocomplete="off">
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="row">
+                                                            <div class="col-md-2">
+                                                                <div class="form-group">
+                                                                    <label for="im_percent">Passing %</label>
+                                                                    <input type="text" name="im_percent" id="im_percent" value="<?= ($rowim['percent'] ?? ''); ?>" class="form-control" placeholder="Passing %" autocomplete="off">
+                                                                </div>
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                 </div>
                                                 <div class="tab-pane" id="graduation">
                                                     <div class="form-wrapper">
-                                                        <h1>Graduation</h1>
+                                                        <div class="row">
+                                                            <div class="col-md-6">
+                                                                <div class="form-group">
+                                                                    <label for="institute_name">Institute Name</label>
+                                                                    <input type="text" name="institute_name" id="institute_name" value="<?= ($rowgd['institute_name'] ?? ''); ?>" class="form-control" placeholder="Institute Name with address" autocomplete="off">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-md-2">
+                                                                <div class="form-group">
+                                                                    <label for="course_name">Course Name</label>
+                                                                    <input type="text" name="course_name" id="course_name" value="<?= ($rowgd['course_name'] ?? ''); ?>" class="form-control" placeholder="Course Name" autocomplete="off">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-md-2">
+                                                                <div class="form-group">
+                                                                    <label for="branch_name">Branch Name</label>
+                                                                    <input type="text" name="branch_name" id="branch_name" value="<?= ($rowgd['branch_name'] ?? ''); ?>" class="form-control" placeholder="Branch Name" autocomplete="off">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-md-2">
+                                                                <div class="form-group">
+                                                                    <label for="enroll_no">Enroll No</label>
+                                                                    <input type="text" name="enroll_no" id="enroll_no" value="<?= ($rowgd['enroll_no'] ?? ''); ?>" class="form-control" placeholder="Enroll No" autocomplete="off">
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="row">
+                                                            <div class="col-md-2">
+                                                                <div class="form-group">
+                                                                    <label for="gd_yop">Year of Passing</label>
+                                                                    <input type="text" name="gd_yop" id="gd_yop" value="<?= ($rowgd['yop'] ?? ''); ?>" class="form-control" placeholder="Year of Passing" autocomplete="off">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-md-2">
+                                                                <div class="form-group">
+                                                                    <label for="aggregate_percent">Passing %</label>
+                                                                    <input type="text" name="aggregate_percent" id="aggregate_percent" value="<?= ($rowgd['aggregate_percent'] ?? ''); ?>" class="form-control" placeholder="Passing %" autocomplete="off">
+                                                                </div>
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
